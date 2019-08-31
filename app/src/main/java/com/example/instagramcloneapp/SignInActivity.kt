cf286681f7045417118210b26e520f87914728a1
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-08-31 21:56:26 +0700

    add sign in UI

--- app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
@@ -1,12 +1,18 @@
 package com.example.instagramcloneapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        btn_sign_up.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/instagramcloneapp/SignUpActivity.kt
@@ -0,0 +1,19 @@
+package com.example.instagramcloneapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        btn_sign_in_link.setOnClickListener{
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+
+    }
+}
