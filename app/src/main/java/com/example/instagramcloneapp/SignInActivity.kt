961c9ebd84544264f6792f5460d201313c320a73
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-09-08 09:35:00 +0700

    Firebase sign In & Out

--- app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
+++ app/src/main/java/com/example/instagramcloneapp/SignInActivity.kt
@@ -1,8 +1,11 @@
 package com.example.instagramcloneapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
@@ -15,6 +18,10 @@ class SignInActivity : AppCompatActivity() {
         btn_sign_up.setOnClickListener{
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+
+        btn_login.setOnClickListener {
+            loginUser()
+        }
     }
 
     override fun onStart() {
@@ -24,6 +31,41 @@ class SignInActivity : AppCompatActivity() {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
+        }
+    }
+    private fun loginUser(){
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        when{
+            TextUtils.isEmpty(email) -> Toast.makeText(this, "email is required.", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this, "password is required.", Toast.LENGTH_LONG).show()
+
+            else -> {
+                val progressDialog = ProgressDialog(this@SignInActivity)
+                progressDialog.setTitle("Login")
+                progressDialog.setMessage("Logging in...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth:FirebaseAuth = FirebaseAuth.getInstance()
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task->
+                    if (task.isSuccessful)
+                    {
+                        progressDialog.dismiss()
+                        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else{
+                        val message = task.exception!!.toString()
+                        Toast.makeText(this, "Error: $message", Toast.LENGTH_LONG).show()
+                        mAuth.signOut()
+                        progressDialog.dismiss()
+                    }
+                }
+            }
         }
     }
 }
